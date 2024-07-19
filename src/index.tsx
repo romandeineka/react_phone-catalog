7f01f93ea62d509863adad09343b64134828c41c
romandeineka <romandeinekaa@gmail.com>
2024-07-19 16:39:33 +0300

    fix hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppProvider } from './AppContext';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <AppProvider>
-    <BrowserRouter basename="/react_phone-catalog">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AppProvider>,
 );
